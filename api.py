9b1f1627a2ae0cd3e50f0b68a4fa869f221db367
josuesantacruz32 <josue.santa.luna@gmail.com>
2024-12-10 23:27:49 -0600

    Version_Final

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+from tempfile import template
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
@@ -5,12 +6,17 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 app.mount("/static",StaticFiles(directory="static/"),name="static")
 
-#@app.get("/")
-#def home():
- #   return "Bienvenidos"
 
+# Ruta para la página principal
 @app.get("/", response_class=HTMLResponse)
-def home():
+async def home():
     with open("templates/index.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return html_content
+
+# Ruta para la tabla
+@app.get("/tabla", response_class=HTMLResponse)
+async def tabla():
+    with open("templates/Tabla.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return html_content
